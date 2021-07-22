412507f249b3bcadd1e29df1531da7a302256d87
Matthew Kocher <mkocher@pivotal.io>
2021-07-22 00:04:57 +0000

    Add protocols to route_destinations
    - this is a little odd because route destinations store http2 but don't
    store tcp as a version. This causes us to have to handle some edge cases
    so there are a lot of specs
    - noticed that internal routes are currently reported to be http though
    they do not logically have a protocol as they're just a DNS entry
    - not using this field yet. next we will use this to decorate the
    routing information of the LRPs.
    - this is a big commit. We regret that we cannot find a good way to make
    it smaller.
    
    Authored-by: Merric de Launey <mdelauney@pivotal.io>

--- app/models/runtime/route.rb
+++ app/models/runtime/route.rb
@@ -183,6 +183,10 @@ module VCAP::CloudController
       domain.shared? && domain.tcp? && port.present? && port > 0
     end
 
+    def protocol
+      self.domain.protocols[0]
+    end
+
     def internal?
       domain.internal
     end
